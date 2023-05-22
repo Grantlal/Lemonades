ae4642a21a090e521dfec9cbebb2826ce2134452
Grant <granttlalonde@gmail.com>
2023-05-22 13:27:02 -0700

    First

---
+++ README.md
@@ -0,0 +1,18 @@
+# Lemonades
+
+Welcome to the real wedding webzone. 
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For a detailed explanation of the build process, read the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main-layout>
+    <p>Hello Everyone!</p>
+    <p>Thank you for coming to celebrate the union of Maria and Grant</p>
+    <p>We'll be using this page to keep track of the calendar, the plans for the week, and since like 20 people asked our registry.</p>
+    <p>We are so absolutely excited for this week, and hope you are too</p>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
