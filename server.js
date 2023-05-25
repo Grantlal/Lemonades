640535dafe45ad5cb349bc6d88e4f0c59f258cef
Grant <granttlalonde@gmail.com>
2023-05-25 15:56:02 -0700

    New node stages

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const fs = require('fs').promises;
 
-const hostname = '127.0.0.1';
-const PORT = 3001;
+const hostname = 'localhost';
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  fs.readFile(__dirname + "/index.html")
+  .then(contents => {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(contents);
+  })
+  .catch(err => {
+    res.writeHead(500);
+    res.end(err);
+    return;
+  });
 });
 
 server.listen(PORT, hostname, () => {
